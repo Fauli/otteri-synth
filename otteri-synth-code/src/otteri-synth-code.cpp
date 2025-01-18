2be5f7bdbef02acf7738ea6780cf78d035584341
fauli <f@sbebe.ch>
2025-01-18 20:21:33 +0100

    moved to platformIO

--- otteri-synth-code/src/otteri-synth-code.cpp
+++ otteri-synth-code/src/otteri-synth-code.cpp
@@ -2,14 +2,66 @@
 #include <Audio.h>
 
 // Define notes
-#define NOTE_C4  261.63
-#define NOTE_D4  293.66
-#define NOTE_E4  329.63
-#define NOTE_F4  349.23
-#define NOTE_G4  392.00
-#define NOTE_A4  440.00
-#define NOTE_B4  493.88
-#define NOTE_C5  523.25
+#define NOTE_C4 (261.63)
+#define NOTE_CS4 (277.18)
+#define NOTE_D4 (293.66)
+#define NOTE_DS4 (311.13)
+#define NOTE_E4 (329.63)
+#define NOTE_F4 (349.23)
+#define NOTE_FS4 (369.99)
+#define NOTE_G4 (392.00)
+#define NOTE_GS4 (415.30)
+#define NOTE_A4 (440.00)
+#define NOTE_AS4 (466.16)
+#define NOTE_B4 (493.88)
+#define NOTE_C5 (523.25)
+#define NOTE_CS5 (554.37)
+#define NOTE_D5 (587.33)
+#define NOTE_DS5 (622.25)
+#define NOTE_E5 (659.25)
+#define NOTE_F5 (698.46)
+#define NOTE_FS5 (739.99)
+#define NOTE_G5 (783.99)
+#define NOTE_GS5 (830.61)
+#define NOTE_A5 (880.00)
+#define NOTE_AS5 (932.33)
+#define NOTE_B5 (987.77)
+#define NOTE_C6 (1046.50)
+#define NOTE_CS6 (1108.73)
+#define NOTE_D6 (1174.66)
+#define NOTE_DS6 (1244.51)
+#define NOTE_E6 (1318.51)
+#define NOTE_F6 (1396.91)
+#define NOTE_FS6 (1479.98)
+#define NOTE_G6 (1567.98)
+#define NOTE_GS6 (1661.22)
+#define NOTE_A6 (1760.00)
+#define NOTE_AS6 (1864.66)
+#define NOTE_B6 (1975.53)
+#define NOTE_C7 (2093.00)
+#define NOTE_CS7 (2217.46)
+#define NOTE_D7 (2349.32)
+#define NOTE_DS7 (2489.02)
+#define NOTE_E7 (2637.02)
+#define NOTE_F7 (2793.83)
+#define NOTE_FS7 (2959.96)
+#define NOTE_G7 (3135.96)
+#define NOTE_GS7 (3322.44)
+#define NOTE_A7 (3520.00)
+#define NOTE_AS7 (3729.31)
+#define NOTE_B7 (3951.07)
+#define NOTE_C8 (4186.01)
+#define NOTE_CS8 (4434.92)
+#define NOTE_D8 (4698.63)
+#define NOTE_DS8 (4978.03)
+#define NOTE_E8 (5274.04)
+#define NOTE_F8 (5587.65)
+#define NOTE_FS8 (5919.91)
+#define NOTE_G8 (6271.93)
+#define NOTE_GS8 (6644.88)
+#define NOTE_A8 (7040.00)
+#define NOTE_AS8 (7458.62)
+#define NOTE_B8 (7902.13)
 
 // Audio library components
 AudioSynthWaveform       waveform1;           // Waveform generator
@@ -30,14 +82,14 @@ Bounce buttons[numButtons] = {
   Bounce(8, 5)
 };
 
-float notes[numButtons - 1] = {NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4, NOTE_C5};
+float notes[numButtons - 1] = {NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6};
 int currentPreset = 0;
 
 void setup() {
   Serial.begin(9600);
   AudioMemory(15);
   sgtl5000_1.enable();
-  sgtl5000_1.volume(0.6);
+  sgtl5000_1.volume(0.75);
   
   // Initialize buttons
   for (int i = 0; i < numButtons; ++i) {
@@ -71,7 +123,8 @@ void loop() {
     }
     if (buttons[i].rose()) {
       envelope1.noteOff();
-      Serial.println("Note stopped.");
+      Serial.print("Note stopped: ");
+      Serial.println(notes[i]);
     }
   }
   
