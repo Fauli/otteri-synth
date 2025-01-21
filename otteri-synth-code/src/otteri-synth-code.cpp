2a603fcc69546fe4e31e8a72a1ed41d3e2c0f127
fauli <f@sbebe.ch>
2025-01-21 23:02:05 +0100

    addeed cents

--- otteri-synth-code/src/otteri-synth-code.cpp
+++ otteri-synth-code/src/otteri-synth-code.cpp
@@ -69,38 +69,45 @@ void stopNote(int note);
 void changeToPreset(int preset);
 
 // Audio library components
-AudioSynthWaveform waveform1[3];     // Waveform generator
-AudioSynthWaveform waveform2[3];     // Waveform generator
-AudioSynthWaveform waveform3[3];     // Waveform generator
-AudioMixer4 mixer1[3];               // Mixer to combine waveforms
-AudioEffectEnvelope envelope1[3];    // Envelope generator
-AudioFilterStateVariable filter1[3]; // Filter to modify the waveform
-AudioOutputI2S i2s1;                 // Audio output over I2S
+AudioSynthWaveform waveform1[3];  // Waveform generator
+AudioSynthWaveform waveform2[3];  // Waveform generator
+AudioSynthWaveform waveform3[3];  // Waveform generator
+AudioMixer4 mixer1[3];            // Mixer to combine waveforms
+AudioEffectEnvelope envelope1[3]; // Envelope generator
 
 AudioMixer4 outputMixer;
+
+AudioOutputI2S i2s1; // Audio output over I2S
+
+// Somehow this fails, resp. crashes? TODO: find out why?
+// AudioConnection patchCord1(waveform1[0], 0, mixer1[0], 0);
+// AudioConnection patchCord2(waveform2[0], 0, mixer1[0], 1);
+// AudioConnection patchCord3(waveform3[0], 0, mixer1[0], 2);
+// AudioConnection patchCord4(mixer1[0], 0, ladder1[0], 0);
+// AudioConnection patchCord5(dc1[0], 0, envelopeFilter1[0], 0);
+// AudioConnection patchCord6(envelopeFilter1[0], 0, ladder1[0], 1);
+// AudioConnection patchCord7(ladder1[0], 0, envelope1[0], 0);
+// AudioConnection patchCord8(envelope1[0], 0, outputMixer, 0);
 
 AudioConnection patchCord1(waveform1[0], 0, mixer1[0], 0);
 AudioConnection patchCord2(waveform2[0], 0, mixer1[0], 1);
 AudioConnection patchCord3(waveform3[0], 0, mixer1[0], 2);
 AudioConnection patchCord4(mixer1[0], envelope1[0]);
-AudioConnection patchCord5(envelope1[0], 0, filter1[0], 0);
-AudioConnection patchCord6(filter1[0], 0, outputMixer, 0);
+AudioConnection patchCord5(envelope1[0], 0, outputMixer, 0);
 
 AudioConnection patchCord7(waveform1[1], 0, mixer1[1], 0);
 AudioConnection patchCord8(waveform2[1], 0, mixer1[1], 1);
 AudioConnection patchCord9(waveform3[1], 0, mixer1[1], 2);
 AudioConnection patchCord10(mixer1[1], envelope1[1]);
-AudioConnection patchCord11(envelope1[1], 0, filter1[1], 0);
-AudioConnection patchCord12(filter1[1], 0, outputMixer, 1);
+AudioConnection patchCord11(envelope1[1], 0, outputMixer, 1);
 
 AudioConnection patchCord13(waveform1[2], 0, mixer1[2], 0);
 AudioConnection patchCord14(waveform2[2], 0, mixer1[2], 1);
 AudioConnection patchCord15(waveform3[2], 0, mixer1[2], 2);
 AudioConnection patchCord16(mixer1[2], envelope1[2]);
-AudioConnection patchCord17(envelope1[2], 0, filter1[2], 0);
-AudioConnection patchCord18(filter1[2], 0, outputMixer, 2);
+AudioConnection patchCord17(envelope1[2], 0, outputMixer, 2);
 
-AudioConnection patchCord19(outputMixer, 0, i2s1, 0);
+AudioConnection patchCord192(outputMixer, 0, i2s1, 0);
 AudioConnection patchCord20(outputMixer, 0, i2s1, 1);
 
 AudioControlSGTL5000 sgtl5000_1; // Control chip for I2S
@@ -108,6 +115,10 @@ AudioControlSGTL5000 sgtl5000_1; // Control chip for I2S
 int numVoices = 3;
 int noteArray[8];
 int activeNote = 0;
+
+int cents[3] = {0, 0, 0};
+int semitones[3] = {0, 0, 0};
+int octaves = 1;
 
 // Button setup
 const int numButtons = 9;
@@ -152,6 +163,9 @@ void setup() {
  * @param preset The index of the preset configuration to apply.
  */
 void changeToPreset(int preset) {
+    cents[0] = 0;
+    cents[1] = 0;
+    cents[2] = 0;
     switch (preset) {
     case 0:
         for (int i = 0; i < numVoices; i++) {
@@ -167,70 +181,196 @@ void changeToPreset(int preset) {
             envelope1[i].decay(120);
             envelope1[i].sustain(0.7);
             envelope1[i].release(250);
+            cents[0] = -10;
+            cents[1] = 9;
+            cents[2] = 0;
         }
         break;
     case 1:
         for (int i = 0; i < numVoices; i++) {
-            Serial.println("Square");
-            waveform1[i].begin(WAVEFORM_SQUARE);
-            waveform1[i].amplitude(0.9);
-            waveform2[i].begin(WAVEFORM_SQUARE);
-            waveform2[i].amplitude(0.9);
-            waveform3[i].begin(WAVEFORM_TRIANGLE);
-            waveform3[i].amplitude(0.9);
+            Serial.println("Ambient Pad");
+            waveform1[i].begin(WAVEFORM_SINE);
+            waveform1[i].amplitude(0.8);
+            waveform2[i].begin(WAVEFORM_TRIANGLE);
+            waveform2[i].amplitude(0.5);
+            waveform3[i].begin(WAVEFORM_SAWTOOTH);
+            waveform3[i].amplitude(0.3);
 
-            envelope1[i].attack(100);
-            envelope1[i].decay(120);
-            envelope1[i].sustain(0.7);
-            envelope1[i].release(500);
+            envelope1[i].attack(800);
+            envelope1[i].decay(1000);
+            envelope1[i].sustain(0.6);
+            envelope1[i].release(1500);
+            cents[0] = -4;
+            cents[1] = 4;
+            cents[2] = 0;
         }
         break;
     case 2:
         for (int i = 0; i < numVoices; i++) {
-            Serial.println("Sawtooth");
-            waveform1[i].begin(WAVEFORM_SAWTOOTH);
+            Serial.println("Percussive Stab");
+            waveform1[i].begin(WAVEFORM_SQUARE);
             waveform1[i].amplitude(0.9);
-            waveform2[i].begin(WAVEFORM_SAWTOOTH);
+            waveform2[i].begin(WAVEFORM_PULSE);
             waveform2[i].amplitude(0.9);
-            waveform3[i].begin(WAVEFORM_PULSE);
-            waveform3[i].amplitude(0.9);
+            waveform3[i].begin(WAVEFORM_SAMPLE_HOLD);
+            waveform3[i].amplitude(0.4);
 
             envelope1[i].attack(0);
-            envelope1[i].decay(4);
-            envelope1[i].sustain(0.5);
-            envelope1[i].release(10);
+            envelope1[i].decay(200);
+            envelope1[i].sustain(0);
+            envelope1[i].release(100);
         }
         break;
     case 3:
         for (int i = 0; i < numVoices; i++) {
-            Serial.println("Triangle");
-            waveform1[i].begin(WAVEFORM_TRIANGLE);
-            waveform1[i].amplitude(0.9);
+            Serial.println("Deep Bass");
+            waveform1[i].begin(WAVEFORM_SINE);
+            waveform1[i].amplitude(1.0);
             waveform2[i].begin(WAVEFORM_SAWTOOTH);
-            waveform2[i].amplitude(0.9);
-            waveform3[i].begin(WAVEFORM_PULSE);
-            waveform3[i].amplitude(0.9);
+            waveform2[i].amplitude(0.7);
+            waveform3[i].begin(WAVEFORM_SQUARE);
+            waveform3[i].amplitude(0.5);
 
-            envelope1[i].attack(50);
-            envelope1[i].decay(120);
-            envelope1[i].sustain(0.7);
-            envelope1[i].release(250);
+            envelope1[i].attack(5);
+            envelope1[i].decay(400);
+            envelope1[i].sustain(0.8);
+            envelope1[i].release(200);
         }
         break;
     case 4:
         for (int i = 0; i < numVoices; i++) {
-            Serial.println("Pulse");
-            waveform1[i].begin(WAVEFORM_PULSE);
-            waveform1[i].amplitude(0.9);
+            Serial.println("Lead Synth");
+            waveform1[i].begin(WAVEFORM_SAWTOOTH);
+            waveform1[i].amplitude(0.8);
             waveform2[i].begin(WAVEFORM_PULSE);
-            waveform2[i].amplitude(0.9);
-            waveform3[i].begin(WAVEFORM_PULSE);
-            waveform3[i].amplitude(0.9);
+            waveform2[i].amplitude(0.6);
+            waveform3[i].begin(WAVEFORM_TRIANGLE);
+            waveform3[i].amplitude(0.5);
 
             envelope1[i].attack(50);
-            envelope1[i].decay(120);
+            envelope1[i].decay(300);
             envelope1[i].sustain(0.7);
-            envelope1[i].release(250);
+            envelope1[i].release(400);
+        }
+        break;
+    case 5:
+        for (int i = 0; i < numVoices; i++) {
+            Serial.println("Space Whispers");
+            waveform1[i].begin(WAVEFORM_SINE);
+            waveform1[i].amplitude(0.3);
+            waveform2[i].begin(WAVEFORM_TRIANGLE);
+            waveform2[i].amplitude(0.3);
+            waveform3[i].begin(WAVEFORM_TRIANGLE_VARIABLE);
+            waveform3[i].amplitude(0.3);
+            waveform3[i].pulseWidth(0.75); // Variable pulse width for a dynamic texture
+
+            envelope1[i].attack(500);
+            envelope1[i].decay(2000);
+            envelope1[i].sustain(0.5);
+            envelope1[i].release(3000);
+        }
+        break;
+    case 6:
+        for (int i = 0; i < numVoices; i++) {
+            Serial.println("Ethereal Voices");
+            waveform1[i].begin(WAVEFORM_SINE);
+            waveform1[i].amplitude(0.8);
+            waveform2[i].begin(WAVEFORM_TRIANGLE);
+            waveform2[i].amplitude(0.5);
+            waveform3[i].begin(WAVEFORM_SAWTOOTH);
+            waveform3[i].amplitude(0.3);
+
+            envelope1[i].delay(100);    // Delay before starting
+            envelope1[i].attack(1200);  // Very slow attack for a smooth onset
+            envelope1[i].hold(500);     // Hold to sustain the sound
+            envelope1[i].decay(800);    // Slow decay
+            envelope1[i].sustain(0.6);  // Sustained level
+            envelope1[i].release(1500); // Very long release for a gradual fade
+        }
+        break;
+    case 7:
+        for (int i = 0; i < numVoices; i++) {
+            Serial.println("Glitch Machine");
+            waveform1[i].begin(WAVEFORM_SQUARE);
+            waveform1[i].amplitude(0.8);
+            waveform2[i].begin(WAVEFORM_TRIANGLE);
+            waveform2[i].amplitude(0.6);
+            waveform3[i].begin(WAVEFORM_SAMPLE_HOLD);
+            waveform3[i].amplitude(0.5);
+
+            envelope1[i].attack(10);
+            envelope1[i].decay(50);
+            envelope1[i].sustain(0.1);
+            envelope1[i].release(10);
+        }
+        break;
+    case 8:
+        for (int i = 0; i < numVoices; i++) {
+            Serial.println("Retro Arcade");
+            waveform1[i].begin(WAVEFORM_BANDLIMIT_SQUARE);
+            waveform1[i].amplitude(1.0);
+            waveform2[i].begin(WAVEFORM_SQUARE);
+            waveform2[i].amplitude(0.7);
+            waveform3[i].begin(WAVEFORM_SINE);
+            waveform3[i].amplitude(0.4);
+
+            envelope1[i].attack(5);
+            envelope1[i].decay(300);
+            envelope1[i].sustain(0.8);
+            envelope1[i].release(150);
+        }
+        break;
+    case 9:
+        for (int i = 0; i < numVoices; i++) {
+            Serial.println("Cyber Drone");
+            waveform1[i].begin(WAVEFORM_SAWTOOTH);
+            waveform1[i].amplitude(0.9);
+            waveform2[i].begin(WAVEFORM_BANDLIMIT_PULSE);
+            waveform2[i].amplitude(0.9);
+            waveform3[i].begin(WAVEFORM_SAWTOOTH_REVERSE);
+            waveform3[i].amplitude(0.9);
+
+            envelope1[i].attack(100);
+            envelope1[i].decay(1200);
+            envelope1[i].sustain(0.6);
+            envelope1[i].release(2500);
+        }
+        break;
+    case 10:
+        for (int i = 0; i < numVoices; i++) {
+            Serial.println("Cinematic Sweep");
+            waveform1[i].begin(WAVEFORM_SAWTOOTH);
+            waveform1[i].amplitude(0.7);
+            waveform2[i].begin(WAVEFORM_TRIANGLE);
+            waveform2[i].amplitude(0.3);
+            waveform3[i].begin(WAVEFORM_PULSE);
+            waveform3[i].amplitude(0.5);
+            waveform3[i].pulseWidth(0.25);
+
+            envelope1[i].delay(50);    // Start with a delay
+            envelope1[i].attack(500);  // Slow attack for a sweeping effect
+            envelope1[i].hold(100);    // Short hold
+            envelope1[i].decay(400);   // Gradual decay
+            envelope1[i].sustain(0.4); // Lower sustain level for a fading tail
+            envelope1[i].release(800); // Long release for a smooth tail-off
+        }
+        break;
+    case 11:
+        for (int i = 0; i < numVoices; i++) {
+            Serial.println("Staccato Plucks");
+            waveform1[i].begin(WAVEFORM_TRIANGLE);
+            waveform1[i].amplitude(1.0);
+            waveform2[i].begin(WAVEFORM_SQUARE);
+            waveform2[i].amplitude(0.5);
+            waveform3[i].begin(WAVEFORM_SINE);
+            waveform3[i].amplitude(0.3);
+
+            envelope1[i].delay(0);    // No delay
+            envelope1[i].attack(10);  // Very quick attack
+            envelope1[i].hold(5);     // Minimal hold
+            envelope1[i].decay(20);   // Quick decay
+            envelope1[i].sustain(0);  // No sustain
+            envelope1[i].release(50); // Quick release for a sharp pluck
         }
         break;
     }
@@ -252,9 +392,9 @@ void playNote(int noteIndex) {
     noteArray[activeNote] = noteIndex;
     Serial.println("Note: " + String(notes[noteIndex]) + " Playing: " + String(activeNote));
 
-    waveform1[activeNote].frequency(notes[noteIndex] * pow(2, -1 / 1200.0) * pow(2, random(-2, 3) / 1200.0));
-    waveform2[activeNote].frequency(notes[noteIndex] * pow(2, 1 / 1200.0) * pow(2, random(-2, 3) / 1200.0));
-    waveform3[activeNote].frequency(notes[noteIndex] * pow(2, 2 / 1200.0) * pow(2, random(-2, 3) / 1200.0));
+    waveform1[activeNote].frequency(notes[noteIndex] * pow(2, -1 / 1200.0) * pow(2, cents[0] / 1200.0) * pow(2, random(-2, 3) / 1200.0));
+    waveform2[activeNote].frequency(notes[noteIndex] * pow(2, 1 / 1200.0) * pow(2, cents[1] / 1200.0) * pow(2, random(-2, 3) / 1200.0));
+    waveform3[activeNote].frequency(notes[noteIndex] * pow(2, 2 / 1200.0) * pow(2, cents[2] / 1200.0) * pow(2, random(-2, 3) / 1200.0));
 
     envelope1[activeNote].noteOn();
 
@@ -268,14 +408,13 @@ void playNote(int noteIndex) {
  * Deactivates the sound by turning off `envelope1` and logs the action to the Serial console.
  */
 void stopNote(int note) {
-        for (int j = 0; j < numVoices; j++) {
+    for (int j = 0; j < numVoices; j++) {
         if (noteArray[j] == note) {
             envelope1[j].noteOff();
             noteArray[j] = 0;
         }
     }
     Serial.println("Note: " + String(note) + " Stopped");
-
 }
 
 /**
@@ -297,7 +436,7 @@ void loop() {
     // Check for preset change
     buttons[8].update();
     if (buttons[8].fell()) {
-        currentPreset = (currentPreset + 1) % 5;
+        currentPreset = (currentPreset + 1) % 11;
         Serial.print("Preset changed to: ");
         Serial.print(currentPreset);
         changeToPreset(currentPreset);
